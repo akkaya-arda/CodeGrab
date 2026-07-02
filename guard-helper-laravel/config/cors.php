6d5c5bdf69d5513128fcf2403f8a5b7db28d2f48
akkaya-arda <arda.akkaya53@outlook.com>
2026-07-02 23:43:47 +0300

    Fix CORS allowed origins dynamically and update htaccess routing in production build

--- guard-helper-laravel/config/cors.php
+++ guard-helper-laravel/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => [
+        'http://localhost:4200',
+        $_SERVER['HTTP_ORIGIN']
+    ],
 
     'allowed_origins_patterns' => [],
 
